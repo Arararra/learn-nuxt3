e2611af671342b5daa49e5b411b9fef2fe9b3f3d
Rama <mramamaulana60@gmail.com>
2022-04-08 07:31:32 +0700

    try api fetch

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
   <div class="flex flex-col items-center justify-center h-full space-y-4 text-xl">
     <p>Here's a counter:</p>
     <div class="flex items-center justify-center">
-      
       <button @click="counter++" class="px-4 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-700">
         +
       </button>
@@ -11,6 +10,9 @@
         -
       </button>
     </div>
+    <li v-for="(mountain, index) in mountains" :key="index">
+      {{ mountain.title }}
+    </li>
   </div>
 </template>
 
@@ -19,12 +21,12 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   layout: 'custom',
-  setup() {
+  async setup() {
     const counter = ref(0)
+    let mountains = await fetch('https://api.nuxtjs.dev/mountains').then(res => res.json())
 
-    return {
-      counter
-    }
+    return { counter, mountains }
   }
 })
+
 </script>
